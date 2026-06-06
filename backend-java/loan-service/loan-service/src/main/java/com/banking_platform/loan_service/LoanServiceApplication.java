2e78530d9a9176febbb182a646723af590ea0a8d
Bertrand TAFEMPA <bertrandtafempa@gmail.com>
2026-06-06 22:22:33 +0100

    fixed customer service and trying to fix loan service

--- backend-java/loan-service/loan-service/src/main/java/com/banking_platform/loan_service/LoanServiceApplication.java
+++ backend-java/loan-service/loan-service/src/main/java/com/banking_platform/loan_service/LoanServiceApplication.java
@@ -3,12 +3,11 @@ package com.banking_platform.loan_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class LoanServiceApplication {
 
 	public static void main(String[] args) {
