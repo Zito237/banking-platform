b645234a9990283e1c3b30175b604aa42d502441
Sophia Mba <sophiamba17@gmail.com>
2026-06-23 10:58:26 +0100

    Kubernet

---
+++ infra/postgres-init/init.sql
@@ -0,0 +1,7 @@
+-- Création des bases de données pour chaque microservice
+CREATE DATABASE authdb;
+CREATE DATABASE operatordb;
+CREATE DATABASE accountdb;
+CREATE DATABASE customerdb;
+CREATE DATABASE transactiondb;
+CREATE DATABASE loandb;
